c36e4611d97eac3130b812f4d440022daf9a22c0
Yash8141 <yashsolanki1622@gmail.com>
2025-09-20 14:14:37 +0530

    Change in the file

--- project/url-shortner/models/url.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const urlSchema = new mongoose.Schema({
-  shortCode: { type: String },
-  longUrl: { type: String },
-});
-
-export const Url = mongoose.model("shortUrl", urlSchema);
